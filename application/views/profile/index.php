a72077845546b76e1f7225d903deb1dcf009be7a
Anggih Septiawan <albertanggih@gmail.com>
2019-08-12 18:13:32 +0700

    add menu user

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -4,6 +4,7 @@
 	<img src="<?= base_url() ?>myassets/img/img1.JPG" class="rounded-circle mx-auto d-block mb-2" alt="img-profile">
 	<p class="mb-0 text-center">Nama : <?= $this->session->userdata('nama') ?></p>
 	<p class="text-center">Email &nbsp;: <?= $this->session->userdata('email') ?></p>
-	<a href="<?= base_url('post_iklan') ?>" class="btn btn-primary w-100 rounded-pill mb-2">POST IKLAN</a>
+	<a href="<?= base_url('post_iklan') ?>" class="btn btn-primary w-100 rounded-pill mb-1">POST IKLAN</a>
+	<a href="<?= base_url('user') ?>" class="btn btn-primary w-100 rounded-pill mb-3">LIHAT USER</a>
 	<a href="<?=base_url("auth/login/out")?>" class="btn btn-primary w-100 rounded-pill">LOGOUT</a>
 </div>
---
+++ application/views/user/index.php
@@ -0,0 +1,45 @@
+<div class="accordion" id="accordionExample">
+    <div class="card">
+        <div class="card-header" id="headingOne">
+        <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Username
+            </button>
+        </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-4">
+                    <p>Nama</p>
+                </div>
+                <div class="col-8">
+                    <p>: Username</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <p>Email</p>
+                </div>
+                <div class="col-8">
+                    <p style="word-wrap: normal;">: Username@gmail.com</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <p>Tanggal gabung</p>
+                </div>
+                <div class="col-8">
+                    <p>: 12 Desember 2018</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <a class="btn btn-danger w-100 rounded-pill" href="#">BLOCK</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
